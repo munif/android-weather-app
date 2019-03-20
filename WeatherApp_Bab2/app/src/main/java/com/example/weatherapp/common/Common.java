5748b1c5ef09448a25d609511ca35824a150eaf5
Abdul Munif <munif.if@gmail.com>
2019-03-20 09:28:54 +0700

    Reordering the folder

---
+++ WeatherApp_Bab2/app/src/main/java/com/example/weatherapp/common/Common.java
@@ -0,0 +1,8 @@
+package com.example.weatherapp.common;
+
+import android.location.Location;
+
+public class Common {
+    public static final String APP_ID = "90b4bd92c1e849d4954a5ec5030fb868";
+    public static Location current_location = null;
+}
