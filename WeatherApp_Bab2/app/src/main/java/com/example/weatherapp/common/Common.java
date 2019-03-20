33c3f1bf5e22d38e5c8b84942f63ebb3c511f120
Abdul Munif <munif.if@gmail.com>
2019-03-20 22:58:51 +0700

    Updating for Bab 2

--- WeatherApp_Bab2/app/src/main/java/com/example/weatherapp/common/Common.java
+++ WeatherApp_Bab2/app/src/main/java/com/example/weatherapp/common/Common.java
@@ -2,7 +2,24 @@ package com.example.weatherapp.common;
 
 import android.location.Location;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Common {
     public static final String APP_ID = "90b4bd92c1e849d4954a5ec5030fb868";
     public static Location current_location = null;
+
+    public static String convertUnixToDate(int dt) {
+        Date date = new Date(dt*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
+
+    public static String convertUnixToHour(int sunrise) {
+        Date date = new Date(sunrise*1000L);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm EEE MM yyyy");
+        String formatted = sdf.format(date);
+        return formatted;
+    }
 }
