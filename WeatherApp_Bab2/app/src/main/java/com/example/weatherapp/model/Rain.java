5edd67ea4d92ff390f5f79e218f4ac0e36f39282
Abdul Munif <munif.if@gmail.com>
2019-03-22 22:47:04 +0700

    Updating Bab 3

--- WeatherApp_Bab2/app/src/main/java/com/example/weatherapp/model/Rain.java
+++ WeatherApp_Bab2/app/src/main/java/com/example/weatherapp/model/Rain.java
@@ -1,16 +1,5 @@
 package com.example.weatherapp.model;
 
 public class Rain {
-    private double __invalid_name__3h;
 
-    public Rain() {
-    }
-
-    public double get__invalid_name__3h() {
-        return __invalid_name__3h;
-    }
-
-    public void set__invalid_name__3h(double __invalid_name__3h) {
-        this.__invalid_name__3h = __invalid_name__3h;
-    }
 }
---
+++ WeatherApp_Bab3/app/src/main/java/com/example/weatherapp/ForecastFragment.java
@@ -0,0 +1,139 @@
+package com.example.weatherapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.weatherapp.adapter.WeatherForecastAdapter;
+import com.example.weatherapp.common.Common;
+import com.example.weatherapp.model.WeatherForecastResult;
+import com.example.weatherapp.retrofit.IOpenWeatherMap;
+import com.example.weatherapp.retrofit.RetrofitClient;
+
+import org.w3c.dom.Text;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ForecastFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ForecastFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ForecastFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    CompositeDisposable compositeDisposable;
+    IOpenWeatherMap mService;
+
+    TextView txt_city_name, txt_geo_coord;
+    RecyclerView recycler_forecast;
+
+    static ForecastFragment instance;
+
+    public static ForecastFragment getInstance() {
+        if(instance == null)
+            instance = new ForecastFragment();
+        return instance;
+    }
+
+    public ForecastFragment() {
+        compositeDisposable = new CompositeDisposable();
+        Retrofit retrofit = RetrofitClient.getInstance();
+        mService = retrofit.create(IOpenWeatherMap.class);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View itemView = inflater.inflate(R.layout.fragment_forecast, container, false);
+
+        txt_city_name = (TextView) itemView.findViewById(R.id.txt_city_name);
+        txt_geo_coord = (TextView) itemView.findViewById(R.id.txt_geo_coord);
+
+        recycler_forecast = (RecyclerView) itemView.findViewById(R.id.recycler_forecast);
+        recycler_forecast.setHasFixedSize(true);
+        recycler_forecast.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL,false));
+
+        getForecastWeatherInformation();
+
+
+        return itemView;
+    }
+
+    private void getForecastWeatherInformation() {
+        compositeDisposable.add(mService.getForecastWeatherByLatLng(
+                String.valueOf(Common.current_location.getLatitude()),
+                String.valueOf(Common.current_location.getLongitude()),
+                Common.APP_ID,
+                "metric")
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<WeatherForecastResult>() {
+                    @Override
+                    public void accept(WeatherForecastResult weatherForecastResult) throws Exception {
+                        displayForecastWeather(weatherForecastResult);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                        Log.d("ERROR", "" + throwable.getMessage());
+                    }
+                })
+        );
+    }
+
+    private void displayForecastWeather(WeatherForecastResult weatherForecastResult) {
+        txt_city_name.setText(new StringBuilder(weatherForecastResult.city.name));
+        txt_geo_coord.setText(new StringBuilder(weatherForecastResult.city.coord.toString()));
+
+        WeatherForecastAdapter adapter = new WeatherForecastAdapter(getContext(), weatherForecastResult);
+        recycler_forecast.setAdapter(adapter);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
