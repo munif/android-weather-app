c55c3e03dfdebffb069e52e48aebb84eddaaaa78
Abdul Munif <munif.if@gmail.com>
2019-03-26 13:49:34 +0700

    Update Bab 4

---
+++ WeatherApp_Bab4/app/src/main/java/com/example/weatherapp/adapter/WeatherForecastAdapter.java
@@ -0,0 +1,68 @@
+package com.example.weatherapp.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.weatherapp.R;
+import com.example.weatherapp.common.Common;
+import com.example.weatherapp.model.WeatherForecastResult;
+import com.squareup.picasso.Picasso;
+
+public class WeatherForecastAdapter extends RecyclerView.Adapter<WeatherForecastAdapter.MyViewHolder> {
+
+
+    Context context;
+    WeatherForecastResult weatherForecastResult;
+
+    public WeatherForecastAdapter(Context context, WeatherForecastResult weatherForecastResult) {
+        this.context = context;
+        this.weatherForecastResult = weatherForecastResult;
+    }
+
+    @NonNull
+    @Override
+    public WeatherForecastAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(context).inflate(R.layout.item_weather_forecast, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WeatherForecastAdapter.MyViewHolder holder, int position) {
+
+        // Load icon
+        Picasso.get().load(new StringBuilder("https://openweathermap.org/img/w/")
+                .append(weatherForecastResult.list.get(position).weather.get(0).getIcon())
+                .append(".png").toString()).into(holder.img_weather);
+
+        holder.txt_date_time.setText(new StringBuilder(Common.convertUnixToDate(weatherForecastResult.list.get(position).dt)));
+        holder.txt_description.setText(new StringBuilder(weatherForecastResult.list.get(position).weather.get(0).getDescription()));
+        holder.txt_temperature.setText(new StringBuilder(String.valueOf(weatherForecastResult.list.get(position).main.getTemp())).append("°C"));
+    }
+
+    @Override
+    public int getItemCount() {
+        return weatherForecastResult.list.size();
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView txt_date_time, txt_description, txt_temperature;
+        ImageView img_weather;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            img_weather = (ImageView) itemView.findViewById(R.id.img_weather);
+            txt_date_time = (TextView) itemView.findViewById(R.id.txt_date);
+            txt_description = (TextView) itemView.findViewById(R.id.txt_description);
+            txt_temperature = (TextView) itemView.findViewById(R.id.txt_temperature);
+
+        }
+    }
+}
